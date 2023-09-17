ae4c8af3daac1093d5aee6dc617b564d85ef1365
mayssam-Alsakhen <mayssamalsakhen17@gmail.com>
2023-09-17 21:54:23 +0300

    fixing

--- components/HeaderH.js
+++ components/HeaderH.js
@@ -1,4 +1,4 @@
-import TextAnimation from "react-text-animations";
+// import TextAnimation from "react-text-animations";
 import Button from './Reusable/Button'
 import { useState, useEffect } from "react";
 
@@ -13,7 +13,7 @@ export default function HeaderH() {
       <div className="  bg-[url('../public/home.jpg')] w-full h-[700px]  bg-cover bg-center bg-no-repeat">
         <div className=" w-full h-full bg-black bg-opacity-30 flex  flex-col justify-center items-center sm:justify-start">
           <div className=" text-white w-fit sm:w-[60%] text-5xl md:text-3xl font-bold p-9 h-[40%] flex items-center ">
-            <TextAnimation.Flip
+            {/* <TextAnimation.Flip
               target="Merciful"
               text={["Missing PERSON", "FOUND PERSON", "COMMENT TO HELP"]}
               animation={{
@@ -24,7 +24,7 @@ export default function HeaderH() {
               loop={true}
             >
               ADD A Merciful
-            </TextAnimation.Flip>
+            </TextAnimation.Flip> */}
           </div>
   {add?
    
